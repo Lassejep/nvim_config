2aa4c982d2b3fea39e0f44e8608a4f7a5fbaa5b5
lassejep <lasse.jepsen@hotmail.com>
2025-04-21 17:26:06 +0200

    feat(config): add VectorCode integration and refactor opts to function

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -7,45 +7,53 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-telescope/telescope.nvim",
 	},
-	opts = {
-		strategies = {
-			inline = {
-				adapter = "deepseek",
-			},
-			chat = {
-				selection_strategy = "telescope",
-				adapter = "deepseek",
-				slash_commands = {
-					["gitfiles"] = {
-						description = "List git files",
-						---@param chat CodeCompanion.Chat
-						callback = function(chat)
-							local handle = io.popen("git ls-files")
-							if handle ~= nil then
-								local result = handle:read("*a")
-								handle:close()
-								chat:add_reference({ role = "user", content = result }, "git", "<git_files>")
-							else
-								return vim.notify(
-									"No git files available",
-									vim.log.levels.INFO,
-									{ title = "CodeCompanion" }
-								)
-							end
-						end,
-						opts = {
-							contains_code = false,
+	opts = function()
+		return {
+			strategies = {
+				inline = {
+					adapter = "deepseek",
+				},
+				chat = {
+					selection_strategy = "telescope",
+					adapter = "deepseek",
+					tools = {
+						vectorcode = {
+							description = "Run VectorCode to retrieve the project context.",
+							callback = require("vectorcode.integrations").codecompanion.chat.make_tool({}),
 						},
 					},
-				},
-				keymaps = {
-					close = {
-						modes = { n = "q", i = "<C-c>" },
+					slash_commands = {
+						["gitfiles"] = {
+							description = "List git files",
+							---@param chat CodeCompanion.Chat
+							callback = function(chat)
+								local handle = io.popen("git ls-files")
+								if handle ~= nil then
+									local result = handle:read("*a")
+									handle:close()
+									chat:add_reference({ role = "user", content = result }, "git", "<git_files>")
+								else
+									return vim.notify(
+										"No git files available",
+										vim.log.levels.INFO,
+										{ title = "CodeCompanion" }
+									)
+								end
+							end,
+							opts = {
+								contains_code = false,
+							},
+						},
 					},
+					keymaps = {
+						close = {
+							modes = { n = "q", i = "<C-c>" },
+						},
+					},
+					show_token_count = true,
 				},
-				show_token_count = true,
 			},
-		},
-	},
+		}
+	end,
 	keys = { { "<leader>cc", vim.cmd.CodeCompanionChat, desc = "[C]odeCompanion[C]hat" } },
 }
