f9197c79e8d23d0e023c51aa576329b4008a0cff
lassejep <lasse.jepsen@hotmail.com>
2025-05-07 14:57:48 +0200

    chore(treesitter): configure core syntax support and features

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,4 +2,16 @@ return {
   -- Treesitter plugin, for syntax highlighting.
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc", "markdown_inline" },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.install").prefer_git = true
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
